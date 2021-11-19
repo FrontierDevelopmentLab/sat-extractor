5836f50b256bc610c50d902ba40bb020c9f9c7c5
Chris Arderne <chris@rdrn.me>
2021-11-19 17:46:27 +0000

    Make tile IDs globally unique

--- src/satextractor/tiler/tiler.py
+++ src/satextractor/tiler/tiler.py
@@ -5,7 +5,7 @@ from typing import Union
 import shapely
 from satextractor.models import Tile
 from sentinelhub import CRS
-from sentinelhub import UtmZoneSplitter
+from sentinelhub import UtmGridSplitter
 
 
 def split_region_in_utm_tiles(
@@ -24,19 +24,27 @@ def split_region_in_utm_tiles(
     Returns:
         [List[Tile]]: The Tiles representing each of the boxes
     """
-    utm_zone_splitter = UtmZoneSplitter([region], crs, bbox_size)
-    crs_bboxes = utm_zone_splitter.get_bbox_list()
-    info_bboxes = utm_zone_splitter.get_info_list()
+    utm_splitter = UtmGridSplitter([region], crs, bbox_size)
+    crs_bboxes = utm_splitter.get_bbox_list()
+    info_bboxes = utm_splitter.get_info_list()
 
-    tiles = [
-        Tile(
-            id="_".join(
-                [str(box.crs.epsg), str(info["index_x"]), str(info["index_y"])],
+    assert (
+        bbox_size[0] == bbox_size[1]
+    ), "bbox_size sides should be equal size, i.e. square"
+    size = bbox_size[0]
+
+    tiles = []
+    for info, box in zip(info_bboxes, crs_bboxes):
+        # tile ids are globally unique and take the format shown below
+        zone = info["utm_zone"]
+        row = info["utm_row"]
+        x, y = (int(v / size) for v in box.lower_left)
+        tiles.append(
+            Tile(
+                id=f"{zone}_{row}_{size}_{x}_{y}",
+                epsg=box.crs.epsg,
+                bbox=(box.min_x, box.min_y, box.max_x, box.max_y),
             ),
-            epsg=box.crs.epsg,
-            bbox=(box.min_x, box.min_y, box.max_x, box.max_y),
         )
-        for info, box in zip(info_bboxes, crs_bboxes)
-    ]
 
     return tiles
